b10e20e4d503a4c631f8e19929a40f163b227fab
ZX_12 <veezyveek@gmail.com>
2018-05-17 05:41:05 -0400

    updates for pascal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes }  from "@angular/router";
 import { FormsModule }    from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { SignupComponent } from './signup/signup.component';
 
-import { AppRoutingModule }     from './app-routing.module';
+import { RouterModule, Routes }     from '@angular/router';
+
+const appRoutes: Routes = [
+  {path: 'signup', component: SignupComponent }
+]
 
 @NgModule({
   declarations: [
@@ -16,9 +19,14 @@ import { AppRoutingModule }     from './app-routing.module';
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   bootstrap: [AppComponent]
 })
+
+
 
 export class AppModule { }
